c99c740fef5524d7d4868f611f4862996bbce1e2
connorjl <cjleahy95@googlemail.com>
2017-11-19 03:55:47 +0100

    Whatever

--- train.py
+++ train.py
@@ -51,6 +51,8 @@ if __name__ == "__main__":
         print("=> loaded checkpoint '{}' (epoch {})".format(resume_path, checkpoint['epoch']))
     model.cuda()
 
+    print(model.primary_capsules.shape())
+
     for i in range(start_epoch, num_epochs):
         for e in train_dataloader:
             model.zero_grad()
