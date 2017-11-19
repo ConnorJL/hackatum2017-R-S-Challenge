f60827558f010dfe2dbbdb241ba85dd59b36063c
connorjl <cjleahy95@googlemail.com>
2017-11-19 03:15:17 +0100

    Easy peasy, lemons have been squeezied

--- dataset.py
+++ dataset.py
@@ -36,7 +36,7 @@ class RSDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         labels = np.zeros(59)
-        print(idx)
-        labels[self.labels[self.elements[idx].labels[0].name]] = 1
+        if not len(self.elements[idx].labels) == 0:
+            labels[self.labels[self.elements[idx].labels[0].name]] = 1
         labels = torch.FloatTensor(labels)
         sample = {"image": image, "labels": labels}
